dcfe23c9109041b92fef5f67c62ac9fdce7b9e48
ShiftInsert <shmisco@gmail.com>
2018-12-07 10:31:55 +0100

    Search/Replace fields added, 2 font sizes added, TAB-order corrected

--- proga.py
+++ proga.py
@@ -13,18 +13,25 @@ class Example(QWidget):
 
     def initUI(self):
         self.yaml_config=config_init('r')
-        self.font_size = 10
-        self.font_sizediff = 2
+        self.font_size_m = 10
+        self.font_size_s = 8
+        
         ''' Setting up grid, fields to enter data.
-            self.path           - path to file
+            self.pat h           - path to file
             self.neededcolumns  - collumns for parsed report
             self.columnparse    - collumn to parse data
             self.delimeter      - delimeter
             self.blacklist      - blacklist
             self.whitelist      - whitelist
+            self.search         - search pattern
+            self.replace        - replace pattern
         '''
         grid = QGridLayout()                                            # установка сетки
         grid.setSpacing(10)
+        grid.setColumnStretch(0, 4)
+        grid.setColumnStretch(1, 4)
+        grid.setColumnStretch(2, 4)
+        grid.setColumnStretch(3, 4)
         self.setLayout(grid)
         self.setGeometry(400, 300, 750, 300)                            # установка окна
         self.setWindowTitle('CSV parser')
@@ -33,43 +40,50 @@ class Example(QWidget):
         self.pathLabel.setText('Path to file:')
         grid.addWidget(self.pathLabel, 0, 0)
         font = self.pathLabel.font()  # lineedit current font
-        font.setPointSize(self.font_size - self.font_sizediff)  # change it's size
+        font.setPointSize(self.font_size_s)  # change it's size
         self.pathLabel.setFont(font)  # set font
 
         self.path = QLineEdit()                                         # путь к файлу
         self.path.setPlaceholderText("Path to source file:")
         font = self.path.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
+        font.setPointSize(self.font_size_m)  # change it's size
         self.path.setFont(font)  # set font
         self.path.setText(self.yaml_config['input_file'])
-        grid.addWidget(self.path,1,0,1,4)
+        grid.addWidget(self.path,1,0,1,3)
+        
+        self.btnopen = QPushButton('Open', self)                        # кнопка для открытия файла
+        font = self.btnopen.font()  # lineedit current font
+        font.setPointSize(self.font_size_m)  # change it's size
+        self.btnopen.setFont(font)  # set font
+        grid.addWidget(self.btnopen, 1, 3)
+        self.btnopen.clicked.connect(self.openFileNameDialog)           # connect procedure to button
 
         self.neededcolumnsLabel = QLabel(self)
         self.neededcolumnsLabel.setText('Needed columns:')
         grid.addWidget(self.neededcolumnsLabel, 2, 0)
         font = self.neededcolumnsLabel.font()  # lineedit current font
-        font.setPointSize(self.font_size - self.font_sizediff)  # change it's size
+        font.setPointSize(self.font_size_s)  # change it's size
         self.neededcolumnsLabel.setFont(font)  # set font
 
         self.neededcolumns = QLineEdit()                                # Needed columns
         self.neededcolumns.setPlaceholderText("2 3 9")
         font = self.neededcolumns.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
+        font.setPointSize(self.font_size_m)  # change it's size
         self.neededcolumns.setFont(font)  # set font
         self.neededcolumns.setText(self.yaml_config['needed_cols'])
-        grid.addWidget(self.neededcolumns, 3, 0)
+        grid.addWidget(self.neededcolumns, 3, 0, 1, 1)
 
         self.columnparseLabel = QLabel(self)
         self.columnparseLabel.setText('Column to parse:')
         grid.addWidget(self.columnparseLabel, 2, 2)
         font = self.columnparseLabel.font()  # lineedit current font
-        font.setPointSize(self.font_size - self.font_sizediff)  # change it's size
+        font.setPointSize(self.font_size_s)  # change it's size
         self.columnparseLabel.setFont(font)  # set font
 
         self.columnparse = QLineEdit()                                  # Column to parse
         self.columnparse.setPlaceholderText("9")
         font = self.columnparse.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
+        font.setPointSize(self.font_size_m)  # change it's size
         self.columnparse.setFont(font)  # set font
         self.columnparse.setText(self.yaml_config['col_to_parse'])
         grid.addWidget(self.columnparse, 3, 2)
@@ -78,12 +92,12 @@ class Example(QWidget):
         self.delimeterLabel.setText('Delimiter:')
         grid.addWidget(self.delimeterLabel, 2, 3)
         font = self.delimeterLabel.font()  # lineedit current font
-        font.setPointSize(self.font_size - self.font_sizediff)  # change it's size
+        font.setPointSize(self.font_size_s)  # change it's size
         self.delimeterLabel.setFont(font)  # set font
         
         self.delimiter = QLineEdit()                                    # Delimeter
         font = self.path.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
+        font.setPointSize(self.font_size_m)  # change it's size
         self.path.setFont(font)  # set font
         self.delimiter.setAlignment(Qt.AlignCenter)
         self.delimiter.setText(self.yaml_config['delimit'])
@@ -93,46 +107,71 @@ class Example(QWidget):
         self.whitelistLabel.setText('Whitelist regex: (all lines with this content will stay, empty whitelist = all lines will stay)')
         grid.addWidget(self.whitelistLabel, 4, 0)
         font = self.whitelistLabel.font()  # lineedit current font
-        font.setPointSize(self.font_size - self.font_sizediff)  # change it's size
+        font.setPointSize(self.font_size_s)  # change it's size
         self.whitelistLabel.setFont(font)  # set font
 
         self.whitelist = QPlainTextEdit()                               # White list
         self.whitelist.setTabChangesFocus(True)
         self.whitelist.setPlaceholderText("Examples:\nResult")
         font = self.whitelist.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
+        font.setPointSize(self.font_size_m)  # change it's size
         self.whitelist.setFont(font)  # set font
         self.whitelist.setPlainText("\n".join(self.yaml_config['whitelist']))
-        grid.addWidget(self.whitelist, 5, 0, 1, 5)
+        grid.addWidget(self.whitelist, 5, 0, 1, 4)
         
         self.blacklistLabel = QLabel(self)
         self.blacklistLabel.setText('Blacklist regex: (all lines with this content will disappear)')
         grid.addWidget(self.blacklistLabel, 6, 0)
         font = self.blacklistLabel.font()  # lineedit current font
-        font.setPointSize(self.font_size - self.font_sizediff)  # change it's size
+        font.setPointSize(self.font_size_s)  # change it's size
         self.blacklistLabel.setFont(font)  # set font
         
         self.blacklist = QPlainTextEdit()                               # Black list
         self.blacklist.setTabChangesFocus(True)
         self.blacklist.setPlaceholderText("Examples:\n^$\nResults:")
         font = self.blacklist.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
+        font.setPointSize(self.font_size_m)  # change it's size
         self.blacklist.setFont(font)  # set font
         self.blacklist.setPlainText("\n".join(self.yaml_config['blacklist']))
-        grid.addWidget(self.blacklist, 7, 0, 1, 5)
+        grid.addWidget(self.blacklist, 7, 0, 1, 4)
+        
+        self.searchLabel = QLabel(self)
+        self.searchLabel.setText('Search regex:')
+        grid.addWidget(self.searchLabel, 8, 0)
+        font = self.searchLabel.font()  # lineedit current font
+        font.setPointSize(self.font_size_s)  # change it's size
+        self.searchLabel.setFont(font)  # set font
 
-        self.btnopen = QPushButton('Open', self)                        # кнопка для открытия файла
-        font = self.btnopen.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
-        self.btnopen.setFont(font)  # set font
-        grid.addWidget(self.btnopen, 1, 4)
-        self.btnopen.clicked.connect(self.openFileNameDialog)           # connect procedure to button
+        self.search = QPlainTextEdit()                                  # Search pattern
+        self.search.setTabChangesFocus(True)
+        self.search.setPlaceholderText("!!!!")
+        font = self.search.font()  # lineedit current font
+        font.setPointSize(self.font_size_m)  # change it's size
+        self.search.setFont(font)  # set font
+        self.search.setPlainText("\n".join(self.yaml_config['search_pattern']))
+        grid.addWidget(self.search, 9, 0, 2, 2)
+
+        self.replaceLabel = QLabel(self)
+        self.replaceLabel.setText('Replace regex:')
+        grid.addWidget(self.replaceLabel, 8, 2)
+        font = self.replaceLabel.font()  # lineedit current font
+        font.setPointSize(self.font_size_s)  # change it's size
+        self.replaceLabel.setFont(font)  # set font
+
+        self.replace = QPlainTextEdit()                                 # Replace pattern
+        self.replace.setTabChangesFocus(True)
+        self.replace.setPlaceholderText("????")
+        font = self.replace.font()  # lineedit current font
+        font.setPointSize(self.font_size_m)  # change it's size
+        self.replace.setFont(font)  # set font
+        self.replace.setPlainText("\n".join(self.yaml_config['replace_pattern']))
+        grid.addWidget(self.replace, 9, 2, 2, 2)
 
         self.btnrun = QPushButton('Run', self)                          # кнопка для запуска программы
         font = self.btnrun.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
+        font.setPointSize(self.font_size_m)  # change it's size
         self.btnrun.setFont(font)  # set font
-        grid.addWidget(self.btnrun, 12, 3)
+        grid.addWidget(self.btnrun, 12, 2)
         
         self.btnrun.clicked.connect(self.parser)                        # connect procedure to button
         if self.path.text() == '':
@@ -140,9 +179,9 @@ class Example(QWidget):
 
         self.btnexit = QPushButton('Exit', self)                             # exit button
         font = self.btnexit.font()  # lineedit current font
-        font.setPointSize(self.font_size)  # change it's size
+        font.setPointSize(self.font_size_m)  # change it's size
         self.btnexit.setFont(font)  # set font
-        grid.addWidget(self.btnexit, 12, 4)
+        grid.addWidget(self.btnexit, 12, 3)
         self.btnexit.clicked.connect(QCoreApplication.instance().quit)
 
         # self.show()
